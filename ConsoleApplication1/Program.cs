add50125c39c9e79f3873130107ede720b506aae
Vitaliy <Vitaliy@DESKTOP-3HS4847>
2019-08-18 17:39:37 +0300

    18.08.19_17.39

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,65 +13,66 @@ namespace ConsoleApplication1
     {
         static void Main()
         {
-           
-             Console.WriteLine("Какое задание загрузить: ");
-             int a = Convert.ToInt32(Console.ReadLine());
-             switch (a)
-             {
-                 case 1:
-                     Test.Dz_1();
-                     break;
-
-                 case 2:
-                     Test.Dz_2();
-                     break;
-             }
-                                
+            Test.Print();
+                        
             Console.ReadKey();
         }
 
        
         static void Dz_1()
+             /*
+             Пользователь вводит N целых чисел, необходимо найти минимально и максимально число среди введеных пользователем
+             */
         {
             // ввод чисел
             Console.WriteLine("Сколько чисел хотите сравнить?");
             int K = Convert.ToInt32(Console.ReadLine());
 
-            int[] nums = new int[K];
-            Console.WriteLine($"Введите {K} чисел");
-            for (int i = 0; i < nums.Length; i++)
+            if (K > 0)
             {
-                Console.Write("{0}-е число: ", i + 1);
-                nums[i] = Int32.Parse(Console.ReadLine());
-            }
-
-            // сортировка
-            int temp;
-            for (int i = 0; i < nums.Length - 1; i++)
-            {
-                for (int j = i + 1; j < nums.Length; j++)
+                int[] nums = new int[K];
+                Console.WriteLine($"Введите {K} чисел");
+                for (int i = 0; i < nums.Length; i++)
                 {
-                    if (nums[i] > nums[j])
+                    Console.Write("{0}-е число: ", i + 1);
+                    nums[i] = Int32.Parse(Console.ReadLine());
+                }
+
+                // сортировка
+                int temp;
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < nums.Length; j++)
                     {
-                        temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
+                        if (nums[i] > nums[j])
+                        {
+                            temp = nums[i];
+                            nums[i] = nums[j];
+                            nums[j] = temp;
+                        }
                     }
                 }
-            }
 
-            // вывод
-            Console.WriteLine("Вывод отсортированного массива");
-            for (int i = 0; i < nums.Length; i++)
-            {
-                Console.WriteLine(nums[i]);
+                // вывод
+                Console.WriteLine("Вывод отсортированного массива");
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    Console.WriteLine(nums[i]);
+                }
+                Console.WriteLine($"min: {nums[0]}");
+                Console.WriteLine($"max: {nums[K - 1]}");
             }
-            Console.WriteLine($"min: {nums[0]}");
-            Console.WriteLine($"max: {nums[K - 1]}");
-            
+            else
+            {
+                Console.WriteLine($"Указаное число '{K}' не допустимо для сравнения");
+            }
         }
 
         static void Dz_2()
+            /*
+             Написать программу вывода таблицы char символов с их hex представлением в пользовательском диапазоне 
+             пользователь указывает начало и конец диапазона (диапазон значений 0 - 65 535!)
+            */
         {
             Console.OutputEncoding = Encoding.UTF8;
 
@@ -82,6 +83,7 @@ namespace ConsoleApplication1
             int y = Convert.ToInt32(Console.ReadLine());
 
             //проверка значений от пользователя
+            
             if (x >= char.MinValue && x <= char.MaxValue && y <= char.MaxValue)
             {
                 for (int i = 0; y >= x; i++)
@@ -92,11 +94,45 @@ namespace ConsoleApplication1
                 }
 
             }
-
             else
                 Console.WriteLine("Вы превысили диапазон значений :(");
 
          }
+        static void Print()
+            /*
+             метод для вывода на консоль заданий
+            */
+        {
+            for (;;)
+            { 
+                Console.WriteLine("Какое задание загрузить: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        Test.Dz_1();
+                        Console.WriteLine("Нажмите любую клавишу для очистки консоли");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 2:
+                        Test.Dz_2();
+                        Console.WriteLine("Нажмите любую клавишу для очистки консоли");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    default:
+                        Console.WriteLine("Введите 1 или 2!\nНажмите любую клавишу для очистки консоли");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+
+            }
+
+        }
 
     }
    
