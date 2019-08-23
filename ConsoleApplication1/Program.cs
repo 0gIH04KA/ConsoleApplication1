b374f21b92b6b0228f0e970f78be8a55e76c1f61
Vitaliy <Vitaliy@DESKTOP-3HS4847>
2019-08-23 15:59:08 +0300

    Release

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,26 +13,136 @@ namespace ConsoleApplication1
     {
         static void Main()
         {
-            
+
             Print();
-                        
+       
             Console.ReadKey();
         }
 
-       
+
         static void Dz_1()
-             /*
-             Пользователь вводит N целых чисел, необходимо найти минимально и максимально число среди введеных пользователем
-             */
+            //пользователь вводит 5 чисел найти МИН и МАХ
+        {
+            Console.WriteLine("Задание первое: ");
+
+            //Ввод данных 
+            Console.Write("Введите 1-e число ");
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите 2-e число ");
+            int Number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите 3-e число ");
+            int Number3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите 4-e число ");
+            int Number4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите 5-e число ");
+            int Number5 = Convert.ToInt32(Console.ReadLine());
+
+            //нахождение МАХ числа
+
+            if (Number1 > Number2 && Number1 > Number2 && Number1 > Number3 && Number1 > Number4 && Number1 > Number5)
+            {
+                Console.WriteLine($"Число: {Number1} больше остальных");
+            }
+
+            else if (Number2 > Number1 && Number2 > Number3 && Number2 > Number4 && Number2 > Number5)
+            {
+                Console.WriteLine($"Число: {Number2} больше остальных");
+            }
+
+            else if (Number3 > Number1 && Number3 > Number2 && Number3 > Number4 && Number3 > Number5)
+            {
+                Console.WriteLine($"Число: {Number3} больше остальных");
+            }
+
+            else if (Number4 > Number1 && Number4 > Number2 && Number4 > Number3 && Number4 > Number5)
+            {
+                Console.WriteLine($"Число: {Number4} больше остальных");
+            }
+
+            else
+            {
+                Console.WriteLine($"Число: {Number5} больше остальных");
+            }
+
+
+            // нахождение МИН числа
+
+            if (Number1 < Number2 && Number1 < Number2 && Number1 < Number3 && Number1 < Number4 && Number1 < Number5)
+            {
+                Console.WriteLine($"Число: {Number1} меньше остальных");
+            }
+
+            else if (Number2 < Number1 && Number2 < Number3 && Number2 < Number4 && Number2 < Number5)
+            {
+                Console.WriteLine($"Число: {Number2} меньше остальных");
+            }
+
+            else if (Number3 < Number1 && Number3 < Number2 && Number3 < Number4 && Number3 < Number5)
+            {
+                Console.WriteLine($"Число: {Number3} меньше остальных");
+            }
+
+            else if (Number4 < Number1 && Number4 < Number2 && Number4 < Number3 && Number4 < Number5)
+            {
+                Console.WriteLine($"Число: {Number4} меньше остальных");
+            }
+
+            else
+            {
+                Console.WriteLine($"Число: {Number5} меньше остальных");
+            }
+
+        }
+
+
+        static void Dz_2()
+            
+             //Написать программу вывода таблицы char символов с их hex представлением в пользовательском диапазоне 
+             //пользователь указывает начало и конец диапазона (диапазон значений 0 - 65 535!)
+            
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //диапазон
+            Console.Write("Укажите диапазон значений (0 - 65 535)\nНачало:");
+            int StartRange = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Конец: ");
+            int EndRange = Convert.ToInt32(Console.ReadLine());
+
+            //проверка значений от пользователя
+            
+            if (StartRange >= char.MinValue && StartRange <= char.MaxValue && EndRange <= char.MaxValue)
+            {
+                for (int i = 0; EndRange >= StartRange; i++)
+                {
+                   char X = Convert.ToChar(StartRange); //конвертация в char
+                    Console.WriteLine($"значение {StartRange} Char символ {X} символ hex {StartRange.ToString("X")} "); //вывод 
+                    StartRange++;
+                }
+
+            }
+            else
+                Console.WriteLine("Вы превысили диапазон значений :(");
+
+         }
+
+        static void Dz_3()
+
+        //Пользователь вводит N целых чисел, необходимо найти минимально и максимально число среди введеных пользователем
+
         {
             // ввод чисел
             Console.WriteLine("Сколько чисел хотите сравнить?");
-            int K = Convert.ToInt32(Console.ReadLine());
+            int AmountNumbers = Convert.ToInt32(Console.ReadLine());
 
-            if (K > 0)
+            if (AmountNumbers > 0)
             {
-                int[] nums = new int[K];
-                Console.WriteLine($"Введите {K} чисел");
+                int[] nums = new int[AmountNumbers];
+                Console.WriteLine($"Введите {AmountNumbers} чисел");
                 for (int i = 0; i < nums.Length; i++)
                 {
                     Console.Write("{0}-е число: ", i + 1);
@@ -61,44 +171,16 @@ namespace ConsoleApplication1
                     Console.WriteLine(nums[i]);
                 }
                 Console.WriteLine($"min: {nums[0]}");
-                Console.WriteLine($"max: {nums[K - 1]}");
+                Console.WriteLine($"max: {nums[AmountNumbers - 1]}");
             }
             else
             {
-                Console.WriteLine($"Указаное число '{K}' не допустимо для сравнения");
+                Console.WriteLine($"Указаное число '{AmountNumbers}' не допустимо для сравнения");
             }
+
         }
 
-        static void Dz_2()
-            /*
-             Написать программу вывода таблицы char символов с их hex представлением в пользовательском диапазоне 
-             пользователь указывает начало и конец диапазона (диапазон значений 0 - 65 535!)
-            */
-        {
-            Console.OutputEncoding = Encoding.UTF8;
 
-            //диапазон
-            Console.Write("Укажите диапазон значений (0 - 65 535)\nНачало:");
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Конец: ");
-            int y = Convert.ToInt32(Console.ReadLine());
-
-            //проверка значений от пользователя
-            
-            if (x >= char.MinValue && x <= char.MaxValue && y <= char.MaxValue)
-            {
-                for (int i = 0; y >= x; i++)
-                {
-                   char X = Convert.ToChar(x); //конвертация в char
-                    Console.WriteLine($"значение {x} Char символ {X} символ hex {x.ToString("X")} "); //вывод 
-                    x++;
-                }
-
-            }
-            else
-                Console.WriteLine("Вы превысили диапазон значений :(");
-
-         }
         static void Print()
             /*
              метод для вывода на консоль заданий
@@ -107,8 +189,10 @@ namespace ConsoleApplication1
             for (;;)
             { 
                 Console.WriteLine("Какое задание загрузить: ");
-                int a = Convert.ToInt32(Console.ReadLine());
-                switch (a)
+                string Text = Console.ReadLine();
+                int NumberTask = Convert.ToInt32(Text);
+
+                switch (NumberTask)
                 {
                     case 1:
                         Dz_1();
@@ -124,8 +208,16 @@ namespace ConsoleApplication1
                         Console.Clear();
                         break;
 
+
+                    case 3:
+                        Dz_3();
+                        Console.WriteLine("Нажмите любую клавишу для очистки консоли");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
                     default:
-                        Console.WriteLine("Введите 1 или 2!\nНажмите любую клавишу для очистки консоли");
+                        Console.WriteLine("На данный момент доступно только 2 домашних задания\n(В дальнейшем планируется рассширение списка приложений)\nНажмите любую клавишу для очистки консоли");
                         Console.ReadKey();
                         Console.Clear();
                         break;
@@ -134,86 +226,7 @@ namespace ConsoleApplication1
             }
 
         }
-
-
-
-
-
-        static void IFFF()
-        {
-            Console.WriteLine("Задание первое: ");
-
-            //Ввод данных 
-            Console.Write("Введите первое число ");
-            int A1 = Convert.ToInt32(Console.ReadLine()) ;
-
-            Console.Write("Введите второе число ");
-            int A2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Введите третье число ");
-            int A3 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Введите четвертое число ");
-            int A4 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Введите пятое число ");
-            int A5 = Convert.ToInt32(Console.ReadLine());
-
-            //нахождение МАХ числа
-
-            if (A1 > A2 && A1 > A2 && A1 > A3 && A1 > A4 && A1 > A5)
-            {
-                Console.WriteLine($"Число: {A1} больше остальных");
-            }
-
-            else if (A2 > A1 && A2 > A3 && A2 > A4 && A2 > A5)
-            {
-                Console.WriteLine($"Число: {A2} больше остальных");
-            }
-
-            else if (A3 > A1 && A3 > A2 && A3 > A4 && A3 > A5)
-            {
-                Console.WriteLine($"Число: {A3} больше остальных");
-            }
-
-            else if (A4 > A1 && A4 > A2 && A4 > A3 && A4 > A5)
-            {
-                Console.WriteLine($"Число: {A4} больше остальных");
-            }
-
-            else
-            {
-                Console.WriteLine($"Число: {A5} больше остальных");
-            }
-
-
-            // нахождение МИН числа
-
-            if (A1 < A2 && A1 < A2 && A1 < A3 && A1 < A4 && A1 < A5)
-            {
-                Console.WriteLine($"Число: {A1} меньше остальных");
-            }
-
-            else if (A2 < A1 && A2 < A3 && A2 < A4 && A2 < A5)
-            {
-                Console.WriteLine($"Число: {A2} меньше остальных");
-            }
-
-            else if (A3 < A1 && A3 < A2 && A3 < A4 && A3 < A5)
-            {
-                Console.WriteLine($"Число: {A3} меньше остальных");
-            }
-
-            else if (A4 < A1 && A4 < A2 && A4 < A3 && A4 < A5)
-            {
-                Console.WriteLine($"Число: {A4} меньше остальных");
-            }
-
-            else
-            {
-                Console.WriteLine($"Число: {A5} меньше остальных");
-            }
-        }
+        
     }
    
 }
